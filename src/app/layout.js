3391522ac01f77158d4298871b32b959da8a3641
sssghulamrasool <seositesoft7@gmail.com>
2023-02-27 12:48:02 +0500

    link add cdn of fontawesone

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+          integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body>
         <main className="microgen_dx">
           <Sidebar />
