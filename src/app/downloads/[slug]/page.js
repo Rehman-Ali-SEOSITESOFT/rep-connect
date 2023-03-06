b6ccabf6b5fa3e6ec61e8e727ae6567f073e59d3
sssghulamrasool <seositesoft7@gmail.com>
2023-03-06 19:57:27 +0500

    download ables material dyanmic pages

--- src/app/downloads/[slug]/page.js
+++ src/app/downloads/[slug]/page.js
@@ -1,13 +1,33 @@
 import BreadCrum from "@/components/breadCrum/BreadCrum";
 import style from "./style.module.css";
+// import { Document, Page } from "react-pdf";
+// import pdfs from "../../../assets/pdf/pdf-file-for-testing.pdf";
 export default function page({ params }) {
-  const { slug } = params;
-  let headingname = slug.split("-").join(" ");
+  // const { slug } = params;
+  // let headingname = slug.split("-").join(" ");
+  // const [numPages, setNumPages] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
+
+  // function onDocumentLoadSuccess({ numPages }) {
+  //   setNumPages(numPages);
+  // }
   return (
     <>
-      <BreadCrum breadHeading={headingname} pageName={headingname} />
+      {/* <BreadCrum breadHeading={headingname} pageName={headingname} /> */}
       <section className={style.single_donwnload_detail}>
         <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="pdf_preview">
+                {/* <Document file={pdfs} onLoadSuccess={onDocumentLoadSuccess}>
+                  <Page pageNumber={pageNumber} />
+                </Document>
+                <p>
+                  Page {pageNumber} of {numPages}
+                </p> */}
+              </div>
+            </div>
+          </div>
           <div className="row">
             <div className="col-md-12">
               <ul className={style.list_views}>
