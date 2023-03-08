8dbdd1f15e9b0e909d0a9d102edd47d630efc24e
sssghulamrasool <seositesoft7@gmail.com>
2023-03-08 18:18:07 +0500

    product category page

---
+++ src/app/cart/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./cart.module.css";
+const page = () => {
+  return (
+    <section className={style.cart__page}>
+      <h2>Cart Page</h2>
+    </section>
+  );
+};
+
+export default page;
--- src/app/product-category/[slug]/page.js
+++ src/app/product-category/[slug]/page.js
@@ -1,24 +1,28 @@
 "use client";
 import AsideWidget from "@/components/asidewidget/AsideWidget";
-import Link from "next/link";
+import BreadCrum from "@/components/breadCrum/BreadCrum";
+import CategoryProducts from "@/components/categoryproducts/CategoryProducts";
 import style from "./slug.module.css";
 export default function Page({ params }) {
   const { slug } = params;
 
   return (
-    <section className={style.productCatagoryPage}>
-      <div className="container-xxl">
-        <div className="row">
-          <div className="col-12">
-            <div
-              className={`${style.main_category_wrapper} d-flex justify-content-between`}
-            >
-              <div className={style.product_listing}>Prouct lising</div>
-              <AsideWidget />
+    <>
+      <BreadCrum breadHeading={slug} pageName={slug} />
+      <section className={style.productCatagoryPage}>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12 p-0">
+              <div
+                className={`${style.main_category_wrapper} d-flex justify-content-between`}
+              >
+                <CategoryProducts />
+                <AsideWidget />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
