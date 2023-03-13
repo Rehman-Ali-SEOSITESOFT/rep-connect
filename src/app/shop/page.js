3186e7bb5f5eae0a610b77f74485ad6ca394c3fc
sssghulamrasool <seositesoft7@gmail.com>
2023-03-13 19:44:54 +0500

    working shop page

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,7 +1,28 @@
+import AsideWidget from "@/components/asidewidget/AsideWidget";
+import BreadCrum from "@/components/breadCrum/BreadCrum";
+import Products from "@/components/products/Products";
 import React from "react";
-
+import style from "./shop.module.css";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <BreadCrum breadHeading="Shop" pageName="Home" subPage="Shop" />
+      <section className={style.shop_page}>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div
+                className={`d-flex justify-content-between  ${style.shop_product_wrapper}`}
+              >
+                <Products />
+                <AsideWidget />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
---
+++ src/components/products/Products.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./products.css";
+const Products = () => {
+  return (
+    <div className="products-shop">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam repellat
+      aperiam voluptates dolor quas est magni quasi aliquid assumenda itaque
+      facilis tempore quidem officia voluptatum sit, omnis totam quod eum.
+      facilis tempore quidem officia voluptatum sit, omnis totam quod eum.
+      facilis tempore quidem officia voluptatum sit, omnis totam quod eum.
+      facilis tempore quidem officia voluptatum sit, omnis totam quod eum.
+    </div>
+  );
+};
+
+export default Products;
