e249f6272845aca716d65368a85bb7a87c4fa8b7
SSSAwais <seositesoft5@gmail.com>
2023-05-17 19:13:55 +0500

    microgen university pages complete

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "./slices/toggleSlice";
 import productSlice from "./slices/productSlice";
 import editProfile from "./slices/editProfile";
+import  popUpToggle  from "./slices/togglePopup";
 export const store = configureStore({
   reducer: {
     menuReducer: menuReducer,
     product: productSlice,
-    editPro : editProfile  
+    editPro : editProfile,  
+    togPop:popUpToggle
   },
 });
