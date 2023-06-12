7dd924b4cc36e3913fbc8beb9f41b95a7ee14dc3
sssghulamrasool <seositesoft7@gmail.com>
2023-06-12 19:27:18 +0500

    working on admin panel

--- src/layout/admin/sidebar/Admin_sidebar.js
+++ src/layout/admin/sidebar/Admin_sidebar.js
@@ -1,11 +1,52 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./admin_sidebar.css";
 import logo from "../../../assets/images/logo/Rep-Connect-Logo-2021-02-admin.svg";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const Admin_sidebar = () => {
+  const pathname = usePathname();
+  const [postMenu, setPostMenu] = useState([
+    {
+      name: "add new",
+      link: "post-addnew",
+    },
+    {
+      name: "category",
+      link: "post-categories",
+    },
+    {
+      name: "tags",
+      link: "post-tags",
+    },
+    {
+      name: "all",
+      link: "/",
+    },
+  ]);
+  const [productMenu, setProductMenu] = useState([
+    {
+      name: "add new",
+      link: "product-addnew",
+    },
+    {
+      name: "category",
+      link: "product-categories",
+    },
+    {
+      name: "tags",
+      link: "product-tags",
+    },
+    {
+      name: "all",
+      link: "",
+    },
+  ]);
+
+  const path = pathname.split("/");
+
   return (
     <aside className="admin-sidebar">
       <div className="logoo">
@@ -17,7 +58,7 @@ const Admin_sidebar = () => {
             <span>Main</span>
           </li>
           <li>
-            <Link href="/admin" className="active">
+            <Link href="/admin">
               <span className="d-icon">
                 <i className="fa-solid fa-gauge"></i>
               </span>
@@ -30,65 +71,101 @@ const Admin_sidebar = () => {
           <li>
             <span className="title-name">
               <span className="d-icon">
-                <i class="fa-solid fa-eye-dropper"></i>
+                <i className="fa-solid fa-eye-dropper"></i>
               </span>
               <span> post</span>
-              <span className="d-icon">
-                <i class="fa-solid fa-angle-down"></i>
+              <span className="d-icon w-100 text-end">
+                <i className="fa-solid fa-angle-down"></i>
               </span>
-              <span className="d-icon">
-                <i class="fa-solid fa-angle-up"></i>
+              <span className="d-icon  w-100 text-end">
+                <i className="fa-solid fa-angle-up"></i>
               </span>
             </span>
-            <ol className="more-list">
+
+            <ol className="more-list ">
               <li>
-                <Link href="/admin/post/addnew"> Add New Post </Link>
+                <Link
+                  href={`/admin/post/post-addnew`}
+                  className={`${path === "post-addnew" ? "active" : null}`}
+                >
+                  add new
+                </Link>
               </li>
               <li>
-                <Link href="/admin/post/categories"> Categories </Link>
+                <Link
+                  href={`/admin/post/post-categories`}
+                  className={`${path === "post-categories" ? "active" : null}`}
+                >
+                  category
+                </Link>
               </li>
               <li>
-                <Link href="/admin/post/tags"> Tags </Link>
+                <Link
+                  href={`/admin/post/post-tags`}
+                  className={`${path === "post-tags" ? "active" : null}`}
+                >
+                  tags
+                </Link>
               </li>
+
               <li>
-                <Link href="/admin/post"> All Post </Link>
+                <Link
+                  href={`/admin/post/`}
+                  className={`${path === undefined ? "active" : null}`}
+                >
+                  all post
+                </Link>
               </li>
+
+              {/* {postMenu.map((elem, index) => {
+                return (
+                  <li key={index}> */}
+              {/* <Link href={`/admin/post/`}>{elem.name}</Link> */}
+              {/* </li>
+                );
+              })} */}
             </ol>
           </li>
           <li>
             <span className="title-name">
               <span className="d-icon">
-                <i class="fa-brands fa-product-hunt"></i>
+                <i className="fa-brands fa-product-hunt"></i>
               </span>
               <span>product</span>
-              <span className="d-icon">
-                <i class="fa-solid fa-angle-down"></i>
+              <span className="d-icon w-100 text-end">
+                <i className="fa-solid fa-angle-down"></i>
               </span>
-              <span className="d-icon">
-                <i class="fa-solid fa-angle-up"></i>
+              <span className="d-icon  w-100 text-end">
+                <i className="fa-solid fa-angle-up"></i>
               </span>
             </span>
+
             <ol className="more-list">
-              <li>
-                <Link href="/admin/product/addnew"> Add New product </Link>
-              </li>
-              <li>
-                <Link href="/admin/product/categories">
-                  {" "}
-                  Categories Proudct{" "}
-                </Link>
-              </li>
-              <li>
-                <Link href="/admin/product/tags"> product Tags </Link>
-              </li>
-              <li>
-                <Link href="/admin/product/tags"> product Tags </Link>
-              </li>
-              <li>
-                <Link href="/admin/product"> All product </Link>
-              </li>
+              {productMenu.map((elem, index) => {
+                console.log(
+                  path[2] === "product"
+                    ? path[3] === elem.link
+                      ? "active"
+                      : null
+                    : null
+                );
+                console.log(elem.link);
+                return (
+                  <li key={index}>
+                    <Link
+                      href={`/admin/product/${elem.link}`}
+                      // className={`${
+                      //   path[2] === "product" || path[3] === elem.link
+                      // }`}
+                    >
+                      {" "}
+                      {elem.name}{" "}
+                    </Link>
+                  </li>
+                );
+              })}
             </ol>
-          </li>{" "}
+          </li>
         </ul>
       </div>
     </aside>
