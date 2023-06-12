08c7e4ed45b481ea865ac317a66fd25a87815d8a
sssghulamrasool <seositesoft7@gmail.com>
2023-06-12 16:03:34 +0500

    working on admin panel

--- src/layout/admin/sidebar/Admin_sidebar.js
+++ src/layout/admin/sidebar/Admin_sidebar.js
@@ -1,18 +1,45 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import "./admin_sidebar.css";
+import logo from "../../../assets/images/logo/Rep-Connect-Logo-2021-02-admin.svg";
+import Image from "next/image";
 
 const Admin_sidebar = () => {
   return (
     <aside className="admin-sidebar">
-      <div className="logoo"></div>
+      <div className="logoo">
+        <Image src={logo} alt="rep-coonect" className="img-fluid" />
+      </div>
       <div className="menu-list">
         <ul>
-          <li>
-            <Link href="/admin">Dashboard</Link>
+          <li className="short-head">
+            <span>Main</span>
           </li>
           <li>
-            <span> post</span>
+            <Link href="/admin" className="active">
+              <span className="d-icon">
+                <i className="fa-solid fa-gauge"></i>
+              </span>
+              <span>Dashboard</span>
+            </Link>
+          </li>
+          <li className="short-head">
+            <span>Pages</span>
+          </li>
+          <li>
+            <span className="title-name">
+              <span className="d-icon">
+                <i class="fa-solid fa-eye-dropper"></i>
+              </span>
+              <span> post</span>
+              <span className="d-icon">
+                <i class="fa-solid fa-angle-down"></i>
+              </span>
+              <span className="d-icon">
+                <i class="fa-solid fa-angle-up"></i>
+              </span>
+            </span>
             <ol className="more-list">
               <li>
                 <Link href="/admin/post/addnew"> Add New Post </Link>
@@ -29,7 +56,18 @@ const Admin_sidebar = () => {
             </ol>
           </li>
           <li>
-            <span> product</span>
+            <span className="title-name">
+              <span className="d-icon">
+                <i class="fa-brands fa-product-hunt"></i>
+              </span>
+              <span>product</span>
+              <span className="d-icon">
+                <i class="fa-solid fa-angle-down"></i>
+              </span>
+              <span className="d-icon">
+                <i class="fa-solid fa-angle-up"></i>
+              </span>
+            </span>
             <ol className="more-list">
               <li>
                 <Link href="/admin/product/addnew"> Add New product </Link>
@@ -50,7 +88,7 @@ const Admin_sidebar = () => {
                 <Link href="/admin/product"> All product </Link>
               </li>
             </ol>
-          </li>
+          </li>{" "}
         </ul>
       </div>
     </aside>
